add3eff6226f2489048ce5ec13161628408eb008
aaaa777 <aaaa777@users.noreply.github.com>
2023-04-10 19:16:04 +0900

    fix launch sh

--- init.sh
+++ init.sh
@@ -6,3 +6,5 @@ pip install --no-cache-dir -r requirements.txt
 
 ln -s $PWD/launch.sh /usr/local/bin/launch.sh
 ln -s $PWD/inu_sanzai.service /etc/systemd/system/inu_sanzai.service
+
+chmod +x /usr/local/bin/launch.sh
