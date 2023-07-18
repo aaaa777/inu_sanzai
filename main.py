37c3c69fd846f6a61624c7dc41f8397299dbb836
aaaa777 <aaaa777@users.noreply.github.com>
2023-07-18 16:21:16 +0000

    [add] excluding suffix

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ TOKEN = os.environ.get('TOKEN')
 re_date = re.compile('[0-1]?\d\/[0-3]?\d')
 re_money = re.compile('([1-9][0-9\,]*)')
 re_removes = [
-    re.compile('\d+[回|人|年]'),
+    re.compile('\d+[回|人|年|本|杯|月|日]'),
 ]
 re_replaces = [
     (re.compile('万'), '0000+'),
